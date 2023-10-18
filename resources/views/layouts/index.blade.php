fe6f6db61472a2e1775c9b674c190f84033885ee
Lucas Ramalho <businessramalho@gmail.com>
2023-10-18 17:04:24 -0300

    feature main layout

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Quero Trampar </title>
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+
+            body {
+                background-color: #1E1E1E;
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
